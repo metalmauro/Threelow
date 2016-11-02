809eacbc3b0956ec3a76278eb6cfade36c3dfab0
Matt Mauro <matt.mauro@gmail.com>
2016-11-02 13:52:25 -0400

    created project. First commit

---
+++ Threelow/DSix.h
@@ -0,0 +1,13 @@
+//
+//  DSix.h
+//  Threelow
+//
+//  Created by Matthew Mauro on 2016-11-02.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DSix : Question
+
+@end
---
+++ Threelow/DSix.m
@@ -0,0 +1,13 @@
+//
+//  DSix.m
+//  Threelow
+//
+//  Created by Matthew Mauro on 2016-11-02.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import "DSix.h"
+
+@implementation DSix
+
+@end
